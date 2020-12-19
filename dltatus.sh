404776eec10dfe6f46b9167b6e97a1939c07e801
Brandon Rodmel <sp4c@dl06.mpwteach.com>
2020-12-19 05:11:25 +0000

    First commit of my bash script

--- dltatus.sh
+++ dltatus.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
+USERS=" dl01 dl02 dl03 dl04 dl05 dl06 dl07 dl08 dl09 dl10 dl11"
 
-ping
+for user in $USERS ; do
+
+  ping $user -c 1 2>&1 >/dev/null
+
+  if [ $? = 0 ] ; then
+
+	echo $user ": alive"
+
+  else
+
+	echo $user ": dead"
+
+  fi
+
+done
 
 exit 0
